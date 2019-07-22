ced002c730ccd927a7d65063ac66591f0e8873c1
Avi Vahl <avi.vahl@wix.com>
2019-07-22 14:15:28 +0300

    examples: refactor file-server ix

--- examples/example-file-server/test/file-server-driver.ts
+++ examples/example-file-server/test/file-server-driver.ts
@@ -1,4 +1,4 @@
-import { ConsoleMessage, Page } from 'puppeteer';
+import { Page } from 'puppeteer';
 
 export class FileServerDriver {
     public static async getFromRoot(root: Page) {
@@ -11,26 +11,8 @@ export class FileServerDriver {
     }
 
     public async getTestContentDiv(page: Page) {
-        const elem = await page.$eval('#testdiv', element => element.textContent);
-        if (elem) {
-            return elem;
-        }
-        return null;
-    }
-
-    public getConsoleData(page: Page, timeout: number = 2000): Promise<ConsoleMessage> {
-        let wasResolved = false;
-        return new Promise((resolve, reject) => {
-            page.on('console', msg => {
-                wasResolved = true;
-                resolve(msg);
-            });
-
-            setTimeout(() => {
-                if (!wasResolved) {
-                    reject('Timeout when retrieving messages');
-                }
-            }, timeout);
-        });
+        const elem = await page.waitForSelector('#testdiv');
+        const textContent = await elem.getProperty('textContent');
+        return textContent.jsonValue();
     }
 }
