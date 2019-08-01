0065b93567ab31991e45b563f4336fb49535c694
Avi Vahl <avi.vahl@wix.com>
2019-08-01 17:56:06 +0300

    Refactors

--- packages/core/src/run-engine-app.ts
+++ packages/core/src/run-engine-app.ts
@@ -15,56 +15,34 @@ export interface IFeatureLoader {
     resolvedContexts: Record<string, string>;
 }
 
-export type ConfigLoader = () => Promise<TopLevelConfig>;
-
 export interface IRunEngineAppOptions {
     featureName?: string | null;
-    configName?: string | null;
     featureLoaders: Record<string, IFeatureLoader>;
-    configLoaders: Record<string, ConfigLoader>;
-    overrideConfig?: TopLevelConfig;
+    config?: TopLevelConfig;
 }
 
-export async function runEngineApp({
-    featureName,
-    configName,
-    featureLoaders,
-    configLoaders,
-    overrideConfig = []
-}: IRunEngineAppOptions) {
+export async function runEngineApp({ featureName, featureLoaders, config = [] }: IRunEngineAppOptions) {
     const featureNames = Object.keys(featureLoaders);
-    const configNames = Object.keys(configLoaders);
-
     featureName = featureName || featureNames[0];
-    configName = configName || configNames[0];
 
     const rootFeatureDef = featureLoaders[featureName];
     if (!rootFeatureDef) {
         throw new Error(`cannot find feature "${featureName}". available features: ${featureNames.join(', ')}`);
     }
 
-    const loadConfig = configLoaders[configName];
-
-    if (!loadConfig) {
-        throw new Error(
-            `cannot find configuration "${configName}". available configurations: ${configNames.join(', ')}`
-        );
-    }
-
-    const featuresToLoad = [
+    const [runningFeature] = await Promise.all([
         rootFeatureDef.load(),
         ...rootFeatureDef.depFeatures.map(depName => featureLoaders[depName].load())
-    ];
+    ]);
 
-    const [config, rootFeature] = await Promise.all<TopLevelConfig | SomeFeature>([loadConfig(), ...featuresToLoad]);
+    const engine = new RuntimeEngine([
+        COM.use({ config: { contextMappings: rootFeatureDef.resolvedContexts } }),
+        ...config
+    ]).run(runningFeature);
 
     return {
-        runningFeature: rootFeature as SomeFeature,
-        engine: new RuntimeEngine([
-            COM.use({ config: { contextMappings: rootFeatureDef.resolvedContexts } }),
-            ...(config as TopLevelConfig),
-            ...overrideConfig
-        ]).run(rootFeature as SomeFeature)
+        engine,
+        runningFeature
     };
 }
 
