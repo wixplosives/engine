66a8b6261bd8fb7f2a3b5ac606c0a28fe78f158e
RomanYarik <48176607+RomanYarik@users.noreply.github.com>
2019-08-27 12:58:43 +0300

    Scripts: publish and run published packages (#31)

--- packages/scripts/test/fixtures/engine-multi-feature/feature/app.feature.ts
+++ packages/scripts/test/fixtures/engine-multi-feature/feature/app.feature.ts
@@ -1,4 +1,4 @@
-import _3rdParty, { MAIN } from '3rd-party/3rd-party.feature';
+import _3rdParty, { MAIN } from '@fixture/3rd-party/3rd-party.feature';
 import { Config, Feature, Slot } from '@wixc3/engine-core';
 
 export default new Feature({
