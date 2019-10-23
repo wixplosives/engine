b0e5df44b18058839c46a9da53651addc34a7854
RomanYarik <48176607+RomanYarik@users.noreply.github.com>
2019-10-23 16:00:32 +0300

    Export cli parsing to reuse functions (#58)

---
+++ packages/scripts/src/utils/cli.ts
@@ -0,0 +1,25 @@
+export const kebabCaseToCamelCase = (value: string): string =>
+    value.replace(/[-]\S/g, match => match.slice(1).toUpperCase());
+
+export function parseCliArguments(args: string[]) {
+    const argumentQueue: string[] = [];
+    const options: Record<string, string | boolean> = {};
+    while (args.length) {
+        const currentArgument = args.shift()!;
+        if (currentArgument.startsWith('--')) {
+            if (argumentQueue.length) {
+                options[argumentQueue.shift()!] = argumentQueue.length ? argumentQueue.join(' ') : true;
+                argumentQueue.length = 0;
+            }
+            argumentQueue.push(kebabCaseToCamelCase(currentArgument.slice(2)));
+        } else if (argumentQueue.length) {
+            argumentQueue.push(currentArgument);
+        } else if (args.length && !args[0].startsWith('--')) {
+            args.shift();
+        }
+    }
+    if (argumentQueue.length) {
+        options[argumentQueue.shift()!] = argumentQueue.join(' ');
+    }
+    return options;
+}
---
+++ packages/scripts/src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './cli';
+export * from './instance-of';
+export * from './resolve-from';
+export * from './resolve-packages';
