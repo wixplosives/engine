2ea0e606197f7a19e30e0a7af9f01f50debb2291
Barak Igal <baraki@wix.com>
2019-12-04 14:22:28 +0200

    add test files

---
+++ packages/core/test/node/remote-service-com.spec.ts
@@ -0,0 +1,72 @@
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+import { stub } from 'sinon';
+
+import { BaseHost, Communication, decalreComEmitter } from '../../src';
+
+chai.use(sinonChai);
+
+class EmitterService {
+    listeners = new Set<(s: string) => void>();
+    dispatch(v: string) {
+        for (const s of this.listeners) {
+            s(v);
+        }
+    }
+    on(fn: (...args: any[]) => void) {
+        this.listeners.add(fn);
+    }
+    off(fn: (...args: any[]) => void) {
+        this.listeners.delete(fn);
+    }
+    removeAll() {
+        this.listeners.clear();
+    }
+}
+
+describe('com emitter service', () => {
+    it('(on, off, removeAll)', async () => {
+        const host = new BaseHost();
+        const main = new Communication(host, 'main');
+
+        const host2 = host.open();
+        const main2 = new Communication(host2, 'main2');
+
+        main.registerEnv('main2', host2);
+        const emitterServiceId = { id: 'EmitterService' };
+        const api = new EmitterService();
+        main2.registerAPI(emitterServiceId, api);
+
+        const proxy = main.apiProxy<EmitterService>(Promise.resolve({ id: 'main2' }), emitterServiceId, {
+            ...decalreComEmitter<EmitterService>('on', 'off', 'removeAll')
+        });
+
+        const x = stub();
+
+        await proxy.on(x);
+        await proxy.on(x);
+
+        api.dispatch('');
+
+        expect(api.listeners.size).to.be.equal(1);
+        expect(x.callCount).to.be.equal(2);
+        
+        await proxy.off(x);
+        expect(api.listeners.size).to.be.equal(1);
+
+        await proxy.off(x);
+        expect(api.listeners.size).to.be.equal(0);
+
+        await proxy.on(x);
+        await proxy.on(x);
+
+        api.dispatch('');
+
+        expect(api.listeners.size).to.be.equal(1);
+        expect(x.callCount).to.be.equal(4);
+
+        await proxy.removeAll();
+        expect(api.listeners.size).to.be.equal(0);
+
+    });
+});
