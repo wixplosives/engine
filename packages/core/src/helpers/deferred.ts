0cf71dddbfeee8a9ddbcffc9a2ed27004c372059
Avi Vahl <avi.vahl@wix.com>
2019-12-25 18:20:52 +0200

    core: build/deploy improvements (#93)
    - remove promise-assist and @file-services/utils. inline both deferred and SetMultiMap.
    - moved helpers to their own folder.
    - prepack now creates dev/prod umd bundles in ./umd, with source-maps

---
+++ packages/core/src/helpers/deferred.ts
@@ -0,0 +1,19 @@
+export type PromiseResolveCb<T> = (value?: T | PromiseLike<T>) => void;
+export type PromiseRejectCb = (reason?: any) => void;
+
+export interface IDeferredPromise<T> {
+    promise: Promise<T>;
+    resolve: PromiseResolveCb<T>;
+    reject: PromiseRejectCb;
+}
+
+export function deferred<T = void>(): IDeferredPromise<T> {
+    let resolve!: PromiseResolveCb<T>;
+    let reject!: PromiseRejectCb;
+    const promise = new Promise<T>((res, rej) => {
+        resolve = res;
+        reject = rej;
+    });
+
+    return { promise, resolve, reject };
+}
