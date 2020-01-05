23c12db33a99cf975d8555940d3c40bb3abb0ba9
eladdo <eladdo@wix.com>
2020-01-05 18:33:36 +0200

    tests

---
+++ packages/scripts/test/feature-generator.unit.ts
@@ -0,0 +1,108 @@
+import { createMemoryFs } from '@file-services/memory';
+import fs from '@file-services/node';
+import { expect } from 'chai';
+import generateFeature, { pathToPackagesPath } from '../src/feature-generator';
+import { expectedDirContents, templatesDirContents, FEATURE_NAME } from './mocks/feature-generator.mocks';
+import { readDirectorySync, writeDirectorySync, mapDirectory, enrichContext } from '../src/feature-generator/utils';
+import { compileTemplate } from '../src/utils/string-utils';
+
+describe('Feature Generator', () => {
+    it('reads sync directory', () => {
+        const memoryFs = createMemoryFs(templatesDirContents);
+        const contentsFromFs = readDirectorySync(memoryFs, '/');
+        expect(contentsFromFs).to.eql(templatesDirContents);
+    });
+
+    it('writes sync directory', () => {
+        const memoryFs = createMemoryFs();
+        writeDirectorySync(memoryFs, expectedDirContents, '/');
+        const contentsFromFs = readDirectorySync(memoryFs, '/');
+        expect(contentsFromFs).to.eql(expectedDirContents);
+    });
+
+    it('maps directory content', () => {
+        const dirContent = {
+            'text1.txt': 'text1.txt content',
+            folder: {
+                'text2.txt': 'text2.txt content'
+            }
+        };
+        const mapper = (name: string, content?: string) => ({
+            name: `map-${name}`,
+            content: `map-${content || ''}`
+        });
+
+        const mappedDirectory = mapDirectory(dirContent, mapper);
+
+        expect(mappedDirectory).to.eql({
+            'map-text1.txt': 'map-text1.txt content',
+            'map-folder': {
+                'map-text2.txt': 'map-text2.txt content'
+            }
+        });
+    });
+
+    it('generates a feature directory from templates directory', () => {
+        const memoryFs = createMemoryFs({
+            templates: templatesDirContents
+        });
+
+        generateFeature(memoryFs, {
+            featureName: FEATURE_NAME,
+            targetPath: '/packages',
+            templatesDirPath: '/templates'
+        });
+
+        const featureDir = readDirectorySync(memoryFs, '/packages');
+
+        expect(featureDir).to.eql(expectedDirContents);
+    });
+
+    describe.only('pathToPackagesPath()', () => {
+        it('Goes up in path to packages folder', () => {
+            expect(pathToPackagesPath(fs, '/proj/packages/some-package')).to.equal('proj/packages');
+        });
+
+        it('Adds `packages` to the path if path has no such parent directory', () => {
+            expect(pathToPackagesPath(fs, '/proj')).to.equal('/proj/packages');
+        });
+    });
+
+    describe('enrichContext()', () => {
+        it('keeps original context', () => {
+            const featureName = 'Bla-Bla';
+            const template = '${featureName}';
+            const enrichedContext = enrichContext({ featureName });
+            const transformed = compileTemplate(template)(enrichedContext);
+            const expected = 'Bla-Bla';
+            expect(transformed).to.equal(expected);
+        });
+
+        it('adds camelCase option', () => {
+            const featureName = 'Bla-Bla';
+            const template = '${featureName.camelCase}';
+            const enrichedContext = enrichContext({ featureName });
+            const transformed = compileTemplate(template)(enrichedContext);
+            const expected = 'blaBla';
+            expect(transformed).to.equal(expected);
+        });
+
+        it('adds dashCase option', () => {
+            const featureName = 'Bla-Bla';
+            const template = '${featureName.dashCase}';
+            const enrichedContext = enrichContext({ featureName });
+            const transformed = compileTemplate(template)(enrichedContext);
+            const expected = 'bla-bla';
+            expect(transformed).to.equal(expected);
+        });
+
+        it('adds pascalCase option', () => {
+            const featureName = 'Bla-Bla';
+            const template = '${featureName.pascalCase}';
+            const enrichedContext = enrichContext({ featureName });
+            const transformed = compileTemplate(template)(enrichedContext);
+            const expected = 'BlaBla';
+            expect(transformed).to.equal(expected);
+        });
+    });
+});
---
+++ packages/scripts/test/mocks/feature-generator.mocks.ts
@@ -0,0 +1,31 @@
+export const templatesDirContents = {
+    feature: {
+        '${featureName.dashCase}.feature.ts.tmpl': 'export function ${featureName.pascalCase}Feature() {}'
+    },
+    fixtures: {
+        '${featureName.dashCase}-test.tmpl': {
+            '${featureName.dashCase}-test.feature.ts.tmpl': 'export function ${featureName.pascalCase}TestFeature() {}'
+        }
+    },
+    'index.ts.tmpl': "export * from './feature/${featureName.dashCase}.feature.ts'",
+    'const-file.txt': 'no chanege here: ${featureName.dashCase}',
+    'README.md.tmpl': 'This is readme for ${featureName.camelCase}'
+};
+
+export const FEATURE_NAME = 'Cool-thing';
+
+export const expectedDirContents = {
+    'cool-thing': {
+        feature: {
+            'cool-thing.feature.ts': 'export function CoolThingFeature() {}'
+        },
+        fixtures: {
+            'cool-thing-test': {
+                'cool-thing-test.feature.ts': 'export function CoolThingTestFeature() {}'
+            }
+        },
+        'index.ts': "export * from './feature/cool-thing.feature.ts'",
+        'const-file.txt': 'no chanege here: ${featureName.dashCase}',
+        'README.md': 'This is readme for coolThing'
+    }
+};
