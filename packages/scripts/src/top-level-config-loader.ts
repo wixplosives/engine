096907874a17536c27cacc0c550ceeac4a9acef6
RomanYar <romany@wix.com>
2020-01-23 13:14:58 +0200

    Scripts: fix static config loaders

--- packages/scripts/src/top-level-config-loader.ts
+++ packages/scripts/src/top-level-config-loader.ts
@@ -1,12 +1,7 @@
 import { loader as webpackLoader } from 'webpack';
 
 export default function(this: webpackLoader.LoaderContext) {
-    walkChildModules(require.cache[this.resourcePath], ({ filename }) => {
-        if (!filename.includes('node_modules') && filename.includes(this.rootContext)) {
-            delete require.cache[filename];
-        }
-    });
-    const imported = require(this.resourcePath) as { default?: any };
+    const imported = requireDeepHack(this.resourcePath, this.rootContext);
     walkChildModules(require.cache[this.resourcePath], ({ filename }) => {
         if (!filename.includes('node_modules') && filename.includes(this.rootContext)) {
             this.addDependency(filename);
@@ -26,4 +21,23 @@ function walkChildModules(nodeJsModule: NodeModule, visitor: (module: NodeModule
             walkChildModules(cm, visitor, registryCache);
         });
     }
+}
+
+/**
+ * This all method is a hack that allows fresh requiring modules
+ */
+function requireDeepHack(resourcePath: string, rootContext: string) {
+    const previousCache: Record<string, NodeModule> = {};
+    walkChildModules(require.cache[resourcePath], ({ filename }) => {
+        if (!filename.includes('node_modules') && filename.includes(rootContext)) {
+            previousCache[filename] = require.cache[filename];
+            delete require.cache[filename];
+        }
+    });
+    const imported = require(resourcePath) as { default?: any };
+    for (const [key, nodeModule] of Object.entries(previousCache)) {
+        require.cache[key] = nodeModule;
+    }
+
+    return imported;
 }
