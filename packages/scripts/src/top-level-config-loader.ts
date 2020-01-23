6a216a6fab1ad9e91832b98fde154f72d6b5ee4c
RomanYarik <48176607+RomanYarik@users.noreply.github.com>
2020-01-23 15:08:23 +0200

    Scripts: fix static config loaders (#104)

--- packages/scripts/src/top-level-config-loader.ts
+++ packages/scripts/src/top-level-config-loader.ts
@@ -1,18 +1,13 @@
 import { loader as webpackLoader } from 'webpack';
 
 export default function(this: webpackLoader.LoaderContext) {
-    walkChildModules(require.cache[this.resourcePath], ({ filename }) => {
-        if (!filename.includes('node_modules') && filename.includes(this.rootContext)) {
-            delete require.cache[filename];
-        }
-    });
-    const imported = require(this.resourcePath) as { default?: any };
+    const imported = requireDeepHack(this.resourcePath, this.rootContext);
     walkChildModules(require.cache[this.resourcePath], ({ filename }) => {
         if (!filename.includes('node_modules') && filename.includes(this.rootContext)) {
             this.addDependency(filename);
         }
     });
-    return `export default JSON.parse(${JSON.stringify(JSON.stringify(imported.default || imported))})`;
+    return `export default JSON.parse(${JSON.stringify(JSON.stringify(imported))})`;
 }
 
 function walkChildModules(nodeJsModule: NodeModule, visitor: (module: NodeModule) => void, registryCache = new Set()) {
@@ -26,4 +21,23 @@ function walkChildModules(nodeJsModule: NodeModule, visitor: (module: NodeModule
             walkChildModules(cm, visitor, registryCache);
         });
     }
+}
+
+/**
+ * This all method is a hack that allows fresh requiring modules
+ */
+function requireDeepHack(resourcePath: string, rootContext: string) {
+    const previousCache: Record<string, NodeModule> = {};
+    walkChildModules(require.cache[resourcePath], ({ filename }) => {
+        if (!filename.includes('node_modules') && filename.includes(rootContext)) {
+            previousCache[filename] = require.cache[filename];
+            delete require.cache[filename];
+        }
+    });
+    const imported = require(resourcePath) as { default?: any };
+    for (const [key, nodeModule] of Object.entries(previousCache)) {
+        require.cache[key] = nodeModule;
+    }
+
+    return imported.default ?? imported;
 }
