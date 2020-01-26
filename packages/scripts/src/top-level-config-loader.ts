b2b31d027a6482b52108d9001199eeda9085e5f9
RomanYar <romany@wix.com>
2020-01-26 18:17:38 +0200

    Revert "Scripts: fix static config loaders (#104)"
    This reverts commit 6a216a6fab1ad9e91832b98fde154f72d6b5ee4c.

--- packages/scripts/src/top-level-config-loader.ts
+++ packages/scripts/src/top-level-config-loader.ts
@@ -1,13 +1,18 @@
 import { loader as webpackLoader } from 'webpack';
 
 export default function(this: webpackLoader.LoaderContext) {
-    const imported = requireDeepHack(this.resourcePath, this.rootContext);
+    walkChildModules(require.cache[this.resourcePath], ({ filename }) => {
+        if (!filename.includes('node_modules') && filename.includes(this.rootContext)) {
+            delete require.cache[filename];
+        }
+    });
+    const imported = require(this.resourcePath) as { default?: any };
     walkChildModules(require.cache[this.resourcePath], ({ filename }) => {
         if (!filename.includes('node_modules') && filename.includes(this.rootContext)) {
             this.addDependency(filename);
         }
     });
-    return `export default JSON.parse(${JSON.stringify(JSON.stringify(imported))})`;
+    return `export default JSON.parse(${JSON.stringify(JSON.stringify(imported.default || imported))})`;
 }
 
 function walkChildModules(nodeJsModule: NodeModule, visitor: (module: NodeModule) => void, registryCache = new Set()) {
@@ -21,23 +26,4 @@ function walkChildModules(nodeJsModule: NodeModule, visitor: (module: NodeModule
             walkChildModules(cm, visitor, registryCache);
         });
     }
-}
-
-/**
- * This all method is a hack that allows fresh requiring modules
- */
-function requireDeepHack(resourcePath: string, rootContext: string) {
-    const previousCache: Record<string, NodeModule> = {};
-    walkChildModules(require.cache[resourcePath], ({ filename }) => {
-        if (!filename.includes('node_modules') && filename.includes(rootContext)) {
-            previousCache[filename] = require.cache[filename];
-            delete require.cache[filename];
-        }
-    });
-    const imported = require(resourcePath) as { default?: any };
-    for (const [key, nodeModule] of Object.entries(previousCache)) {
-        require.cache[key] = nodeModule;
-    }
-
-    return imported.default ?? imported;
 }
