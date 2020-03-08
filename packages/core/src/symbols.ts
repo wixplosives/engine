b8b860be196ae911ad347f564da55fb16c818630
RomanYar <romany@wix.com>
2020-03-08 18:53:42 +0200

    Core: changing runtime symbols to use Symbol.for

--- packages/core/src/symbols.ts
+++ packages/core/src/symbols.ts
@@ -1,8 +1,8 @@
-export const RUN = Symbol('RUN');
-export const DISPOSE = Symbol('DISPOSE');
-export const IDENTIFY_API = Symbol('IDENTIFY_API');
-export const CREATE_RUNTIME = Symbol('CREATE');
-export const REGISTER_VALUE = Symbol('REGISTER_VALUE');
-export const SERVICE_CONFIG = Symbol('SERVICE_CONFIG');
-export const RUN_OPTIONS = Symbol('RUN_OPTIONS');
-export const CONFIGURABLE = Symbol('CONFIGURABLE');
+export const RUN = Symbol.for('__ENGINE__RUN');
+export const DISPOSE = Symbol.for('__ENGINE__DISPOSE');
+export const IDENTIFY_API = Symbol.for('__ENGINE__IDENTIFY_API');
+export const CREATE_RUNTIME = Symbol.for('__ENGINE__CREATE');
+export const REGISTER_VALUE = Symbol.for('__ENGINE__REGISTER_VALUE');
+export const SERVICE_CONFIG = Symbol.for('__ENGINE__SERVICE_CONFIG');
+export const RUN_OPTIONS = Symbol.for('__ENGINE__RUN_OPTIONS');
+export const CONFIGURABLE = Symbol.for('__ENGINE__CONFIGURABLE');
