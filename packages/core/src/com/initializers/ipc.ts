a3cc763158868cc59e562424e0e7e6929ab20593
RomanYar <romany@wix.com>
2020-03-17 17:38:42 +0200

    adding IPC initializer

---
+++ packages/core/src/com/initializers/ipc.ts
@@ -0,0 +1,19 @@
+import { EnvironmentInitializer } from '../types';
+
+export function ipcInitializer(): EnvironmentInitializer<{ id: string }> {
+    return async (communication, { env, endpointType }) => {
+        const instanceId = communication.getEnvironmentInstanceId(env, endpointType);
+
+        const host = communication.getEnvironmentHost(env);
+        if (!host) {
+            throw new Error(`IPC hosts should be registered and forked before the initializer run`);
+        }
+
+        communication.registerMessageHandler(host);
+        await communication.envReady(env);
+
+        return {
+            id: instanceId
+        };
+    };
+}
