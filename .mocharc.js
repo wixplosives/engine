7625fae91997f1a8e59759812d7aa18e6ad8ff57
Avi Vahl <avi.vahl@wix.com>
2020-04-26 20:06:33 +0300

    infra: increase default test timeout to 10s there's flakiness in CI around timeouts. checking whether it's correctness or slowness.

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,5 @@ module.exports = {
     require: ['@ts-tools/node/r', 'tsconfig-paths/register'],
     extension: ['js', 'json', 'ts', 'tsx'],
     colors: true,
-    timeout: 5000,
+    timeout: 10000,
 };
