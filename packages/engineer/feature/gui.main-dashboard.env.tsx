cc89bc87339e384146c7e47b6f28454ac4c5e7af
Adam Yahid <adamy@wix.com>
2020-09-09 16:30:57 +0300

    some cleanup to imports

--- packages/engineer/feature/gui.main-dashboard.env.tsx
+++ packages/engineer/feature/gui.main-dashboard.env.tsx
@@ -3,7 +3,7 @@ import { buildEnv } from './build.feature';
 import { socketServerInitializer } from '@wixc3/engine-core/src';
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from '../../scripts/src/engine-dashboard/components/app';
+import { App } from '../engine-dashboard/components/app';
 
 guiFeature.setup(mainDashboardEnv, ({ run }, { COM: { startEnvironment } }) => {
     run(async () => {
