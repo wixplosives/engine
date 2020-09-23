dbeae96baf4d1e1779f345d69792de25a36be63e
Adam Yahid <adamy@wix.com>
2020-09-23 15:46:55 +0300

    combine imports

--- packages/engineer/feature/gui.dev-server.env.ts
+++ packages/engineer/feature/gui.dev-server.env.ts
@@ -4,9 +4,8 @@ import VirtualModulesPlugin from 'webpack-virtual-modules';
 import fs from '@file-services/node';
 import type webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { createEntrypoint } from '@wixc3/engine-scripts';
+import { createEntrypoint, IConfigDefinition } from '@wixc3/engine-scripts';
 import { SetMultiMap } from '@wixc3/engine-core';
-import type { IConfigDefinition } from '@wixc3/engine-scripts';
 
 guiFeature.setup(
     devServerEnv,
