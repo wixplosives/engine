c5354daba59b340d1a0121edca3252b1ea28c593
Adam Yahid <adamy@wix.com>
2020-10-01 12:17:12 +0300

    allow passing of inspect, return running engineer entry as well as engine and dispose

--- packages/engineer/src/utils.ts
+++ packages/engineer/src/utils.ts
@@ -6,7 +6,7 @@ import {
     runNodeEnvironment,
     TopLevelConfigProvider,
 } from '@wixc3/engine-scripts';
-import { RuntimeEngine, BaseHost, TopLevelConfig } from '@wixc3/engine-core';
+import { RuntimeEngine, BaseHost, TopLevelConfig, RuntimeFeature } from '@wixc3/engine-core';
 
 import devServerFeature, { devServerEnv } from '../feature/dev-server.feature';
 import guiFeature from '../feature/gui.feature';
@@ -27,9 +27,10 @@ export interface IStartOptions {
     autoLaunch?: boolean;
     engineerEntry?: string;
     overrideConfig?: TopLevelConfig | TopLevelConfigProvider;
+    inspect?: boolean;
 }
 
-export function startDevServer({
+export async function startDevServer({
     featureName,
     configName,
     httpServerPort = 3000,
@@ -45,9 +46,11 @@ export function startDevServer({
     engineerEntry = 'engineer/gui',
     overrideConfig = [],
     outputPath,
+    inspect,
 }: IStartOptions): Promise<{
     dispose: () => Promise<void>;
     engine: RuntimeEngine;
+    engineerEntry: RuntimeFeature;
 }> {
     const basePath = resolve(__dirname, '../feature');
     const featurePaths = fs.findFilesSync(basePath, {
@@ -57,7 +60,7 @@ export function startDevServer({
 
     const features = loadFeaturesFromPaths(new Set(featurePaths), new Set([basePath]), fs).features;
 
-    return runNodeEnvironment({
+    const { engine, dispose } = await runNodeEnvironment({
         featureName: engineerEntry,
         features: [...features],
         name: devServerEnv.env,
@@ -79,6 +82,7 @@ export function startDevServer({
                     basePath: targetApplicationPath,
                     overrideConfig,
                     outputPath,
+                    inspect,
                 },
             }),
             guiFeature.use({
@@ -88,6 +92,12 @@ export function startDevServer({
             }),
         ],
     });
+
+    return {
+        engine,
+        dispose,
+        engineerEntry: engine.features.get(features.get(engineerEntry).exportedFeature) as RuntimeFeature,
+    };
 }
 
 function preRequire(pathsToRequire: string[], basePath: string) {
