0b3582b6659233874df24922173b11e15f02312d
RomanYar <romany@wix.com>
2020-11-05 10:34:38 +0200

    removing redundent code

--- examples/my-plugin/feature/my-plugin.feature.ts
+++
@@ -1,8 +0,0 @@
-import { Feature } from '@wixc3/engine-core';
-import Compiler from '@example/playground/src/preview/compiler.feature';
-
-export default new Feature({
-    id: 'myPlugin',
-    dependencies: [Compiler],
-    api: {},
-});
--- examples/my-plugin/feature/my-plugin.processing.env.ts
+++
@@ -1,9 +0,0 @@
-import { PROCESSING } from '@example/playground/src/code-editor/code-editor.feature';
-import MyPlugin from './my-plugin.feature';
-
-MyPlugin.setup(PROCESSING, ({}, { preview: { complierExtension } }) => {
-    complierExtension.register({
-        matcher: (content) => content.length % 2 === 0,
-        compile: (content) => content + content[content.length - 1],
-    });
-});
