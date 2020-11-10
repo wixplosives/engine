f32782d5861f7783b2e16b7d863a134136b97f51
RomanYar <romany@wix.com>
2020-11-10 08:49:31 +0200

    another missing parts

--- packages/scripts/test/fixtures/base-web-application/engine.config.js
+++ packages/scripts/test/fixtures/base-web-application/engine.config.js
@@ -3,7 +3,6 @@ module.exports = {
     require: ['@ts-tools/node/r'],
     externalFeatureDefinitions: [
         {
-            featureName: 'application-external',
             packageName: '@fixture/application-external-feature',
         },
     ],
