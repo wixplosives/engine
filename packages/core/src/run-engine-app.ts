3215363a485b907afd82590309f9af92ae7c8635
Adam Yahid <adamy@wix.com>
2020-11-12 15:15:27 +0200

    fix typings, push only if there are init functions

--- packages/core/src/run-engine-app.ts
+++ packages/core/src/run-engine-app.ts
@@ -20,9 +20,8 @@ export const getFeaturesDeep = (feature: Feature) => flattenTree(feature, (f) =>
 export interface IFeatureLoader {
     load: (resolvedContexts: Record<string, string>) => Promise<Feature>;
     preload: (
-        resolveContexts: Record<string, string>,
-        runtimeOptions: Record<string, string | boolean>
-    ) => Promise<Array<(runtimeOptions: Record<string, string | boolean>) => void | Promise<void>>>;
+        resolveContexts: Record<string, string>
+    ) => Promise<Array<(runtimeOptions: Record<string, string | boolean>) => void | Promise<void>>> | undefined;
     depFeatures: string[];
     resolvedContexts: Record<string, string>;
 }
@@ -111,7 +110,10 @@ export class FeatureLoadersRegistry {
         const allPreloadInitFunctions = [];
         for (const featureLoader of featureLoaders) {
             if (featureLoader.preload) {
-                allPreloadInitFunctions.push(...(await featureLoader.preload(this.resolvedContexts, runtimeOptions)));
+                const featureInitFunctions = await featureLoader.preload(this.resolvedContexts);
+                if (featureInitFunctions) {
+                    allPreloadInitFunctions.push(...featureInitFunctions);
+                }
             }
         }
         for (const initFunction of allPreloadInitFunctions) {
