84cb83ea7b3090235321f69b67d8f2673826b9ce
Adam Yahid <adamy@wix.com>
2020-11-30 13:10:58 +0200

    cr

--- examples/preload/feature/non-contextual.procEnv.nodeCtx.preload.ts
+++ examples/preload/feature/non-contextual.procEnv.nodeCtx.preload.ts
@@ -1,5 +1,1 @@
-import nonContextualFeature from './non-contextual.feature';
-import { procEnv } from './contextual.feature';
-
 globalThis.envMessages = [...(globalThis.envMessages ?? []), 'procEnvContextualNoContextPreload'];
-nonContextualFeature.setup(procEnv, () => undefined);
