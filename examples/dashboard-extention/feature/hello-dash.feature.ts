79d87287f8e02c624549f58a14f36bce69c58cbf
RomanYar <romany@wix.com>
2020-12-10 13:52:50 +0200

    abother fixes

---
+++ examples/dashboard-extention/feature/hello-dash.feature.ts
@@ -0,0 +1,8 @@
+import { Feature } from '@wixc3/engine-core';
+import Gui from '@wixc3/engineer/dist/feature/gui.feature';
+
+export default new Feature({
+    id: 'dashboardExt',
+    dependencies: [Gui],
+    api: {},
+});
