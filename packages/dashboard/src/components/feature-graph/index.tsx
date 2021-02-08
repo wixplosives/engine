f4da8daba461988f3cbc76c9e047301c1087da7f
Adam Yahid <adamy@wix.com>
2021-02-08 15:52:34 +0200

    work with react

--- packages/dashboard/src/components/feature-graph/index.tsx
+++ packages/dashboard/src/components/feature-graph/index.tsx
@@ -1,9 +1,17 @@
-import React, { useEffect, memo } from 'react';
+import React, { useEffect } from 'react';
 import * as d3 from 'd3';
+import { classes } from './styles.st.css';
 
 export interface Link {
-    source: string;
-    target: string;
+    source: string | Node;
+    target: string | Node;
+}
+
+interface Node {
+    name: string;
+    id: string;
+    group: number;
+    parent?: Node;
 }
 
 export interface GraphData {
@@ -19,29 +27,185 @@ export interface IFeatureGraphProps {
     selectedFeatureGraph: GraphData;
 }
 
-export const FeatureGraph = memo<IFeatureGraphProps>((selectedFeatureGraph) => {
-    useEffect(() => {
-        const data = [12, 5, 6, 6, 9, 10];
-        const h = 100;
-        const w = 100;
-        const svg = d3.select('#graph_root').append('svg').attr('width', w).attr('height', h).style('margin-left', 100);
+interface Point {
+    x: number;
+    y: number;
+}
 
-        svg.selectAll('rect')
-            .data(data)
+export const FeatureGraph = ({ selectedFeatureGraph }: IFeatureGraphProps) => {
+    useEffect(() => {
+        const diameter = 600;
+        const radius = diameter / 2;
+        const innerRadius = radius - 70;
+
+        const cluster = d3
+            .cluster()
+            .size([360, innerRadius])
+            .separation(function (a, b) {
+                return a.parent == b.parent ? 1 : a.parent?.parent == b.parent?.parent ? 2 : 4;
+            });
+
+        const line = d3.line<Point>().x(xAccessor).y(yAccessor).curve(d3.curveBundle.beta(0.7));
+
+        const svg = d3
+            .select('#graph_root')
+            .attr('width', diameter)
+            .attr('height', diameter)
+            .append('g')
+            .attr('transform', 'translate(' + radius + ',' + radius + ')');
+
+        const idToNode: Record<string, Node> = {};
+
+        selectedFeatureGraph.nodes.forEach(function (n: Node) {
+            idToNode[n.id] = n;
+        });
+
+        selectedFeatureGraph.links.forEach(function (e: Link) {
+            e.source = idToNode[e.source as string];
+            e.target = idToNode[e.target as string];
+        });
+
+        const tree = cluster(d3.hierarchy<any>(chapterHierarchy(selectedFeatureGraph.nodes)));
+
+        const leaves = tree.leaves();
+
+        const paths = selectedFeatureGraph.links.map(function (l) {
+            var source = leaves.filter(function (d) {
+                return d.data === l.source;
+            })[0];
+            var target = leaves.filter(function (d) {
+                return d.data === l.target;
+            })[0];
+            return source.path(target);
+        });
+
+        const link = svg
+            .selectAll('.link')
+            .data(paths)
             .enter()
-            .append('rect')
-            .attr('x', (_d, i) => i * 70)
-            .attr('y', (d) => h - 10 * d)
-            .attr('width', 65)
-            .attr('height', (d) => d * 10)
-            .attr('fill', 'green');
+            .append('path')
+            .attr('class', classes.link)
+            .attr('d', function (d) {
+                return line(d);
+            })
+            .on('mouseover', function (l) {
+                link.style('stroke', null).style('stroke-opacity', null);
+                d3.select(this).style('stroke', '#d62333').style('stroke-opacity', 1);
+                node.selectAll('circle').style('fill', null);
+                node.filter(function (n) {
+                    return n === l[0] || n === l[l.length - 1];
+                })
+                    .selectAll('circle')
+                    .style('fill', 'black');
+            })
+            .on('mouseout', function () {
+                link.style('stroke', null).style('stroke-opacity', null);
+                node.selectAll('circle').style('fill', null);
+            });
+
+        const node = svg
+            .selectAll('.node')
+            .data(tree.leaves())
+            .enter()
+            .append('g')
+            .attr('class', 'node')
+            .attr('transform', function (d) {
+                return 'translate(' + xAccessor(d) + ',' + yAccessor(d) + ')';
+            })
+            .on('mouseover', function (d) {
+                node.style('fill', null);
+                d3.select(this).selectAll('circle').style('fill', 'black');
+                var nodesToHighlight = paths
+                    .map(function (e) {
+                        return e[0] === d ? e[e.length - 1] : e[e.length - 1] === d ? e[0] : 0;
+                    })
+                    .filter(function (d) {
+                        return d;
+                    });
+                node.filter(function (d) {
+                    return nodesToHighlight.indexOf(d) >= 0;
+                })
+                    .selectAll('circle')
+                    .style('fill', '#555');
+                link.style('stroke-opacity', function (link_d) {
+                    return link_d[0] === d || link_d[link_d.length - 1] === d ? 1 : null;
+                }).style('stroke', function (link_d) {
+                    return link_d[0] === d || link_d[link_d.length - 1] === d ? '#d62333' : null;
+                });
+            })
+            .on('mouseout', function () {
+                link.style('stroke-opacity', null).style('stroke', null);
+                node.selectAll('circle').style('fill', null);
+            });
+
+        node.append('circle')
+            .attr('r', 4)
+            .attr('class', classes.node_circle)
+            .append('title')
+            .text(function (d: any) {
+                return d.data.name;
+            });
+
+        node.append('text')
+            .attr('dy', '0.32em')
+            .attr('x', function (d) {
+                return d.x < 180 ? 6 : -6;
+            })
+            .style('text-anchor', function (d: Point) {
+                return d.x < 180 ? 'start' : 'end';
+            })
+            .attr('transform', function (d: Point) {
+                return 'rotate(' + (d.x < 180 ? d.x - 90 : d.x + 90) + ')';
+            })
+            .text(function (d: any) {
+                return `${d.data.group} - ${d.data.id}`;
+            });
+
+        function chapterHierarchy(characters: Array<Node>) {
+            const hierarchy: any = {
+                root: { name: 'root', children: [] },
+            };
+
+            characters.forEach(function (c) {
+                const chapter = c.group;
+                const book = 0;
+                const volume = 0;
+
+                if (!hierarchy[volume]) {
+                    hierarchy[volume] = { name: volume, children: [], parent: hierarchy['root'] };
+                    hierarchy['root'].children.push(hierarchy[volume]);
+                }
+
+                if (!hierarchy[book]) {
+                    hierarchy[book] = { name: book, children: [], parent: hierarchy[volume] };
+                    hierarchy[volume].children.push(hierarchy[book]);
+                }
+
+                if (!hierarchy[chapter]) {
+                    hierarchy[chapter] = { name: chapter, children: [], parent: hierarchy[book] };
+                    hierarchy[book].children.push(hierarchy[chapter]);
+                }
+
+                c.parent = hierarchy[chapter];
+                hierarchy[chapter].children.push(c);
+            });
+
+            return hierarchy['root'];
+        }
+
+        function xAccessor(d: Point) {
+            const angle = ((d.x - 90) / 180) * Math.PI,
+                radius = d.y;
+            return radius * Math.cos(angle);
+        }
+
+        function yAccessor(d: Point) {
+            const angle = ((d.x - 90) / 180) * Math.PI,
+                radius = d.y;
+            return radius * Math.sin(angle);
+        }
     }, []);
-    return (
-        <>
-            <div id="graph_root" />
-            <pre>{JSON.stringify(selectedFeatureGraph)}</pre>
-        </>
-    );
-});
+    return <svg id="graph_root" />;
+};
 
 FeatureGraph.displayName = 'FeatureGraph';
