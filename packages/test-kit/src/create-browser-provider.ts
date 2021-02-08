43586a070cc6a6a30b78cfac4252158d28d71e45
Avi Vahl <avi.vahl@wix.com>
2021-02-08 14:07:56 +0200

    feat(test-kit)!: migrate puppeteer to playwright

--- packages/test-kit/src/create-browser-provider.ts
+++ packages/test-kit/src/create-browser-provider.ts
@@ -1,14 +1,14 @@
-import puppeteer from 'puppeteer';
+import playwright from 'playwright-core';
 
-export function createBrowserProvider(options?: puppeteer.LaunchOptions) {
-    let browser: puppeteer.Browser | undefined;
+export function createBrowserProvider(options?: playwright.LaunchOptions) {
+    let browser: playwright.Browser | undefined;
     return {
         async loadPage(url: string) {
             if (!browser) {
-                browser = await puppeteer.launch(options);
+                browser = await playwright.chromium.launch(options);
             }
             const page = await browser.newPage();
-            await page.goto(url, { waitUntil: 'networkidle0' });
+            await page.goto(url, { waitUntil: 'networkidle' });
             return page;
         },
         async dispose() {
--- packages/test-kit/src/index.ts
+++ packages/test-kit/src/index.ts
@@ -4,4 +4,3 @@ export * from './detached-app';
 export * from './run-environment';
 export * from './types';
 export * from './with-feature';
-export * from './with-feature-playwright';
