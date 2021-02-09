4b6b3f46386d802fa0dbece28c802635b1f98b9e
RomanYar <romany@wix.com>
2021-02-09 08:56:43 +0200

    even prettier tests

--- packages/scripts/test/analyze-feature-module.unit.ts
+++ packages/scripts/test/analyze-feature-module.unit.ts
@@ -3,12 +3,22 @@ import { expect } from 'chai';
 import { analyzeFeatureModule } from '@wixc3/engine-scripts';
 import { evaluateModule } from '@wixc3/engine-scripts/src/utils/evaluate-module';
 import { join } from 'path';
+import type { ExternalDefinition } from '@wixc3/engine-core';
+
+const pathToBasicFeature = join(__dirname, './fixtures/engine-feature/feature/x.feature');
+const pathTofFeatureWithExportedEnvironment = join(__dirname, './fixtures/node-env/feature/x.feature');
+const pathToFeatureWithExportedUsedContexts = join(__dirname, './fixtures/contextual/fixtures/server-env.feature');
+const pathToFeatureWithSingleExternalDefinition = join(__dirname, './fixtures/external-definitions/module-a.feature');
+const pathToFeatureWithMultipleExternalDefinitions = join(
+    __dirname,
+    './fixtures/external-definitions/module-b.feature'
+);
 
 describe('analyzeFeatureModule', function () {
     it('analyzes a simple feature file', () => {
         const {
             children: [featueModule],
-        } = evaluateModule(join(__dirname, './fixtures/engine-feature/feature/x.feature'));
+        } = evaluateModule(pathToBasicFeature);
         const { name } = analyzeFeatureModule(featueModule);
         expect(name).to.eq('x');
     });
@@ -16,7 +26,7 @@ describe('analyzeFeatureModule', function () {
     it('located environment exports', () => {
         const {
             children: [featueModule],
-        } = evaluateModule(join(__dirname, './fixtures/node-env/feature/x.feature'));
+        } = evaluateModule(pathTofFeatureWithExportedEnvironment);
         const { exportedEnvs } = analyzeFeatureModule(featueModule);
         expect(exportedEnvs).to.have.lengthOf(2);
         expect(exportedEnvs.map((e) => e.name)).to.eql(['main', 'server']);
@@ -25,39 +35,33 @@ describe('analyzeFeatureModule', function () {
     it('locates used contexts', () => {
         const {
             children: [featueModule],
-        } = evaluateModule(join(__dirname, './fixtures/contextual/fixtures/server-env.feature'));
+        } = evaluateModule(pathToFeatureWithExportedUsedContexts);
         const { usedContexts } = analyzeFeatureModule(featueModule);
         expect(usedContexts['contextual']).to.eql('server');
     });
 
-    it('locates external dependencies', () => {
+    it('locates external dependencies', async () => {
         const {
             children: [featureAModule],
-        } = evaluateModule(join(__dirname, './fixtures/external-definitions/module-a.feature'));
+        } = evaluateModule(pathToFeatureWithSingleExternalDefinition);
 
         const { externalDefinitions } = analyzeFeatureModule(featureAModule);
-        expect(externalDefinitions).to.eql([
-            {
-                request: 'my-module',
-                globalName: 'MyModule',
-            },
-        ]);
+
+        const { externalModule: moduleAExternalModule } = (await import(pathToFeatureWithSingleExternalDefinition)) as {
+            externalModule: ExternalDefinition;
+        };
+        expect(externalDefinitions).to.eql([moduleAExternalModule]);
 
         const {
             children: [featureBModule],
-        } = evaluateModule(join(__dirname, './fixtures/external-definitions/module-b.feature'));
+        } = evaluateModule(pathToFeatureWithMultipleExternalDefinitions);
+
         const { externalDefinitions: externalBDefinitions } = analyzeFeatureModule(featureBModule);
 
-        expect(externalBDefinitions).to.eql([
-            {
-                globalName: 'MyOtherModule',
-                request: 'my-other-module',
-            },
-            { request: 'mod', globalName: 'Mod' },
-            {
-                globalName: 'AnotherOtherModule',
-                request: 'another-other-module',
-            },
-        ]);
+        const { externalModule, externalModules } = (await import(pathToFeatureWithMultipleExternalDefinitions)) as {
+            externalModule: ExternalDefinition;
+            externalModules: ExternalDefinition[];
+        };
+        expect(externalBDefinitions).to.eql([...externalModules, externalModule]);
     });
 });
