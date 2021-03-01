1da4bcc549a6db0c28774878df1d20a28398a0ef
Ofir Levitan <eifr0980@gmail.com>
2021-03-01 15:55:06 +0200

    removed unused comments

--- engine.config.js
+++ engine.config.js
@@ -1,8 +1,6 @@
-// const path = require('path');
 /**
  * @type {import('@wixc3/engine-scripts').EngineConfig}
  */
 module.exports = {
     require: ['@ts-tools/node/r'],
-    // favicon: path.join(__dirname, 'favicon.ico'),
 };
