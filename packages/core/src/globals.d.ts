adda18b76c1480e0f5c14ea3d709f88ab45e3aa7
Avi Vahl <avi.vahl@wix.com>
2021-03-08 20:52:07 +0200

    fix(core): improved node detection - avoid using global process. instead, import it from 'process' and map that to an empty object. "browser" field is picked by bundlers. - also, ensure process.versions.node is truthy, for naive polyfills.

--- packages/core/src/globals.d.ts
+++ packages/core/src/globals.d.ts
@@ -1,4 +1,8 @@
 // used by communication to determine the type of environment
 // we want to avoid having @types/node in engine-core
 // eslint-disable-next-line no-var
-declare var process: { title?: string; type?: string } | undefined;
+
+declare module 'process' {
+    const process: { title?: string; type?: string; versions?: { node?: string } };
+    export = process;
+}
