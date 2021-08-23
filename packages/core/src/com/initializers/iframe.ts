327afe291e03b85ccac3200f301788e9bf4afe49
tomermes <tomerme@wix.com>
2021-08-23 09:54:14 +0300

    fix iframe src

--- packages/core/src/com/initializers/iframe.ts
+++ packages/core/src/com/initializers/iframe.ts
@@ -11,7 +11,7 @@ export interface IIframeInitializerOptions {
     managed?: boolean;
 }
 
-export interface IframeInitializerOptions extends InitializerOptions, IIframeInitializerOptions { }
+export interface IframeInitializerOptions extends InitializerOptions, IIframeInitializerOptions {}
 
 export async function iframeInitializer({
     communication,
@@ -30,7 +30,7 @@ export function deferredIframeInitializer({ communication: com, env: { env, endp
     initialize: (options: IIframeInitializerOptions) => Promise<string>;
 } {
     const instanceId = com.getEnvironmentInstanceId(env, endpointType);
-    const envReadyPromise = com.envReady(instanceId)
+    const envReadyPromise = com.envReady(instanceId);
     return {
         id: instanceId,
         initialize: ({ managed, iframeElement, hashParams, src }: IIframeInitializerOptions) => {
@@ -40,19 +40,19 @@ export function deferredIframeInitializer({ communication: com, env: { env, endp
                 envReadyPromise,
                 instanceId,
                 src:
-                    src ?? managed
+                    src || managed
                         ? defaultHtmlSourceFactory(env, publicPath, hashParams)
                         : defaultSourceFactory(env, publicPath),
             };
             return managed
                 ? startManagedIframe({
-                    ...baseStartIframeParams,
-                    iframe: iframeElement,
-                })
+                      ...baseStartIframeParams,
+                      iframe: iframeElement,
+                  })
                 : startIframe({
-                    ...baseStartIframeParams,
-                    host: iframeElement,
-                });
+                      ...baseStartIframeParams,
+                      host: iframeElement,
+                  });
         },
     };
 }
