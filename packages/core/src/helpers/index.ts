d647472f11a72d8e5d1c59295b13ce39ac4f276f
RomanYar <romany@wix.com>
2021-11-24 15:39:03 +0200

    remove reference from index

--- packages/core/src/helpers/index.ts
+++ packages/core/src/helpers/index.ts
@@ -2,6 +2,5 @@ export * from './deferred';
 export * from './event-emitter';
 export * from './set-multi-map';
 export * from './flatten-tree';
-export * from './disposables';
 export * from './web';
 export * from './serialize-error';
