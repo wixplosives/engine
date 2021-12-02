3c6ade5e78e8bd4d176f28daa3326ea9601cb5fa
RomanYar <romany@wix.com>
2021-12-02 18:38:10 +0200

    fix(com): fixing hanging listeners

--- packages/core/src/com/errors.ts
+++ packages/core/src/com/errors.ts
@@ -5,6 +5,8 @@ export const DUPLICATE_REGISTER = (id: string, type: 'RemoteService' | 'Environm
 export const GLOBAL_REF = (id: string) => `Com with id "${id}" is already running.`;
 export const REMOTE_CALL_FAILED = (environment: string, stack?: string) =>
     `Remote call failed in ${environment}${stack ? `\n${stack}` : ''}`;
+export const ENV_DISCONNECTED = (environment: string) =>
+    `Remote call failed in ${environment} - environment disconnected`;
 export const UNKNOWN_CALLBACK_ID = (message: Message) =>
     `Unknown callback id "${message.callbackId!}" in message:\n${JSON.stringify(message)}`;
 export const CALLBACK_TIMEOUT = (callbackId: string, hostId: string, message: Message) =>
