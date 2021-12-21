6b02d21454a9325675af815a81301019eacde908
RomanYar <romany@wix.com>
2021-12-21 16:56:26 +0200

    refactor(ipc-host): safer process type validation

--- packages/core-node/src/ipc-host.ts
+++ packages/core-node/src/ipc-host.ts
@@ -2,7 +2,7 @@ import { BaseHost, IDisposable, Message } from '@wixc3/engine-core';
 import type { ChildProcess } from 'child_process';
 
 export const isParentProcess = (process: NodeJS.Process | ChildProcess): process is NodeJS.Process => {
-    return !!(process as NodeJS.Process).ppid;
+    return (process as NodeJS.Process).constructor.name === 'process';
 };
 
 export class IPCHost extends BaseHost implements IDisposable {
