d3c13a42def80ef0a0847b65053378b00b84528a
Lior Erez <liorer@wix.com>
2022-01-31 16:18:29 +0100

    Remove redundant text content assignment

--- test-fixtures/engine-runtime-metadata/src/feature/x.main.env.ts
+++ test-fixtures/engine-runtime-metadata/src/feature/x.main.env.ts
@@ -2,7 +2,6 @@ import { socketClientInitializer } from '@wixc3/engine-core';
 import sampleFeature, { client, server } from './x.feature';
 
 sampleFeature.setup(client, ({ runtimeMetadata, run }, { COM: { communication } }) => {
-    document.body.textContent = '';
     run(async () => {
         await socketClientInitializer({ communication, env: server });
         const runtimeMetadataConfig = await runtimeMetadata.getRuntimeMetadata();
