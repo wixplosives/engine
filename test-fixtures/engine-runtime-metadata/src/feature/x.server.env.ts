64b65c6c93cc6de51ba73537d73480f710b6a8aa
Lior Erez <liorer@wix.com>
2022-02-10 13:22:14 +0100

    Fix function name

--- test-fixtures/engine-runtime-metadata/src/feature/x.server.env.ts
+++ test-fixtures/engine-runtime-metadata/src/feature/x.server.env.ts
@@ -2,6 +2,6 @@ import SampleFeature, { server } from './x.feature';
 
 SampleFeature.setup(server, ({}, { runtimeMetadata: { engineerMetadataConfig } }) => {
     return {
-        runtimeMetadata: { getRuntimeMetadata: () => engineerMetadataConfig },
+        runtimeMetadata: { getEngineerMetadata: () => engineerMetadataConfig },
     };
 });
