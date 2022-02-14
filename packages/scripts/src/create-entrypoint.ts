fc76a3f05a51d1ee0dc95d0a7454d5e4b01421fe
Viktor Zhakhalov <viktorz@wix.com>
2022-02-14 18:18:06 +0200

    require.resolve path to default-config loader

--- packages/scripts/src/create-entrypoint.ts
+++ packages/scripts/src/create-entrypoint.ts
@@ -110,7 +110,7 @@ export function createMainEntrypoint({
     externalFeaturesRoute,
     eagerEntrypoint,
     featuresBundleName,
-    configLoaderModuleName = '@wixc3/engine-scripts',
+    configLoaderModuleName = require.resolve('./default-config-loader')
 }: ICreateEntrypointsOptions) {
     const configs = getAllValidConfigurations(getConfigLoaders(configurations, mode, configName), envName);
     return `
