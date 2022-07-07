9283c2cbb1ab7763a5ea737f0a2f39faff400d3c
Oleh Rak <olehr@wix.com>
2022-07-07 15:50:53 +0300

    feat(extend-envs-api): Fixed review comment

--- packages/core/src/com/communication.ts
+++ packages/core/src/com/communication.ts
@@ -339,7 +339,7 @@ export class Communication {
         return this.environments[envName]?.host;
     }
 
-    public getRegisteredEnvironmentNames(): string[] {
+    public getRegisteredEnvironmentInstances(): string[] {
         return Object.keys(this.environments).filter((id) => id !== '*');
     }
 
