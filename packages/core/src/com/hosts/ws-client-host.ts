1a60411739681fae14a67bb553cf5a2e8bc4e1c5
Viktor Zhakhalov <viktorz@wix.com>
2022-07-18 17:54:20 +0300

    Handle url with pathname creating WsClientHost

--- packages/core/src/com/hosts/ws-client-host.ts
+++ packages/core/src/com/hosts/ws-client-host.ts
@@ -11,10 +11,13 @@ export class WsClientHost extends BaseHost {
     constructor(url: string, options?: Partial<SocketOptions>) {
         super();
 
+        const { protocol, host, pathname: path, searchParams } = new URL(url);
+        const query = Object.fromEntries(searchParams as unknown as Iterable<any>);
+
         const { promise, resolve } = deferred();
         this.connected = promise;
 
-        this.socketClient = io(url, { ...options, transports: ['websocket'], forceNew: true });
+        this.socketClient = io(`${protocol}://${host}`, { transports: ['websocket'], forceNew: true, path, query, ...options });
 
         this.socketClient.on('connect', () => {
             this.socketClient.on('message', (data: unknown) => {
