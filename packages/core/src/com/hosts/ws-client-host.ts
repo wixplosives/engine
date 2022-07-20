bc047ce6c4eaf0d8f3de444b0a792d6a415a5bd8
zviktorz <91947555+zviktorz@users.noreply.github.com>
2022-07-20 13:04:18 +0300

    feat: Handle url with pathname creating WsClientHost (#1364)
    Added:
    - Ability to connect to websocket server hosted at some pathname for 
    
    Example:  
    ```ts
    new WsClientHost('wss://manage.wix.com?path=_api/wcs-gateway')
    ```

--- packages/core/src/com/hosts/ws-client-host.ts
+++ packages/core/src/com/hosts/ws-client-host.ts
@@ -11,10 +11,18 @@ export class WsClientHost extends BaseHost {
     constructor(url: string, options?: Partial<SocketOptions>) {
         super();
 
+        const { path, ...query } = Object.fromEntries(new URL(url).searchParams);
+
         const { promise, resolve } = deferred();
         this.connected = promise;
 
-        this.socketClient = io(url, { ...options, transports: ['websocket'], forceNew: true });
+        this.socketClient = io(url, {
+            transports: ['websocket'],
+            forceNew: true,
+            path,
+            query,
+            ...options
+        });
 
         this.socketClient.on('connect', () => {
             this.socketClient.on('message', (data: unknown) => {
