a087d553101641dc4bfe3fe636597cfb90069bdc
alexshe <alexshe@wix.com>
2022-08-29 16:47:18 +0300

    feat: add parameter to sign electron build

--- packages/electron-scripts/src/commands/build.ts
+++ packages/electron-scripts/src/commands/build.ts
@@ -123,10 +123,17 @@ export async function build(options: IBuildCommandOptions): Promise<void> {
         outDir,
     });
 
-    const builderConfig = fs.readJsonFileSync(fs.join(basePath, electronBuilderConfigFileName)) as Configuration;
+    const builderConfig = fs.readJsonFileSync(fs.join(basePath, electronBuilderConfigFileName)) as Configur ation;
+
     if (!sign) {
-        builderConfig.win.certificateSubjectName = null
+        // Disable code signing on windows
+        if (builderConfig.win) {
+            Object.assign(builderConfig.win, {certificateSubjectName: null});
+        }
+        // Disable code signing on mac
+        process.env.CSC_IDENTITY_AUTO_DISCOVERY = 'false';
     }
+
     const extraFiles: (string | FileSet)[] = [
         {
             from: dirname(require.resolve('@wixc3/engine-electron-host/package.json')),
