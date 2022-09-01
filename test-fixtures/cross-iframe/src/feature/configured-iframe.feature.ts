1d52513f5beb1c021d98c575200136cea1f1c9c0
Serhii Zadorozhnyi <serhiiz@wix.com>
2022-09-01 16:04:56 +0200

    test: update example

--- test-fixtures/cross-iframe/src/feature/configured-iframe.feature.ts
+++ test-fixtures/cross-iframe/src/feature/configured-iframe.feature.ts
@@ -2,15 +2,10 @@ import { COM, Config, Environment, Feature } from '@wixc3/engine-core';
 
 import echoFeature from './echo.feature';
 export const mainEnv = new Environment('main', 'window', 'single');
-export const iframeEnv = new Environment('iframe', 'iframe', 'single');
+export const iframeEnv = new Environment('iframe', 'iframe', 'multi');
 
 export default new Feature({
     id: 'managedCrossOriginIframeFeature',
-    api: {
-        config: new Config<{
-            message?: string;
-            origin?: string;
-        }>({}),
-    },
+    api: {},
     dependencies: [COM.asDependency, echoFeature.asDependency],
 });
--- test-fixtures/cross-iframe/src/feature/cross.config.ts
+++
@@ -1,9 +0,0 @@
-import fixture from './configured-iframe.feature';
-
-export default [
-    fixture.use({
-        config: {
-            origin: '//127.0.0.1:3000',
-        },
-    }),
-];
