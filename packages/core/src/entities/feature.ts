c051ed480c076bcf1403f038ab30139313c59f0e
Avi Vahl <avi.vahl@wix.com>
2022-11-19 16:36:21 +0200

    refactor: use deferred from promise-assist newer `@wixc3/common` version no longer has it

--- packages/core/src/entities/feature.ts
+++ packages/core/src/entities/feature.ts
@@ -16,7 +16,8 @@ import type {
     Dependency,
 } from '../types';
 import { AnyEnvironment, Environment, testEnvironmentCollision } from './env';
-import { deferred, IDeferredPromise, SetMultiMap } from '@wixc3/common';
+import { deferred, IDeferredPromise } from 'promise-assist';
+import { SetMultiMap } from '@wixc3/common';
 
 const emptyDispose = { dispose: () => undefined };
 
