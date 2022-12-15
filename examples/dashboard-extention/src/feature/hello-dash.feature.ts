c7d0e8f7a45d0cdd8800bccb968b3eaff04a74b0
Avi Vahl <avi.vahl@wix.com>
2022-12-15 18:50:14 +0200

    infra: remove unused dashboard-extention example

--- examples/dashboard-extention/src/feature/hello-dash.feature.ts
+++
@@ -1,8 +0,0 @@
-import { Feature } from '@wixc3/engine-core';
-import Gui from '@wixc3/engineer/dist/feature/gui.feature';
-
-export default new Feature({
-    id: 'dashboardExt',
-    dependencies: [Gui.asDependency],
-    api: {},
-});
