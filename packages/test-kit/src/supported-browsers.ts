c78fbe189b6663f3c597bc016f101669465eba73
matangeorgi <matang@wix.com>
2023-01-15 11:26:35 +0200

    fix: remove newline from the error message

--- packages/test-kit/src/supported-browsers.ts
+++ packages/test-kit/src/supported-browsers.ts
@@ -8,6 +8,6 @@ export function validateBrowser(name: string): SupportedBrowser {
     if (isValidBrowserName(name)) {
         return name;
     }
-    throw new Error(`Invalid browser name was entered as env var: ${name} \n
+    throw new Error(`Invalid browser name was entered as env var: ${name},
                     Possible options for browsers are: ${supportedBrowsers.join(', ')}.`);
 }
