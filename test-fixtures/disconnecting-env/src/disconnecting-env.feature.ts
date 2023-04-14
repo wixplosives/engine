8342a54098d85c95066933fda090d44f34183986
Oleh Rak <olehr@wix.com>
2023-04-14 15:55:36 +0300

    refactor: improve readability and fix electron-node.spec tests

--- test-fixtures/disconnecting-env/src/disconnecting-env.feature.ts
+++ test-fixtures/disconnecting-env/src/disconnecting-env.feature.ts
@@ -2,16 +2,16 @@ import { Config, Feature, Environment, COM } from '@wixc3/engine-core';
 
 export const serverEnv = new Environment('server', 'node', 'single');
 
-export type ErrorTypeConfig = {
-    errorMode: 'exception' | 'exit' | 'promiseReject' | 'out-of-memory' | 'no-error';
+export type ErrorsConfig = {
+    throwError: 'exception' | 'exit' | 'promise-reject' | false;
     handleUncaught: boolean;
 };
 
 export default new Feature({
     id: 'disconnecting-env',
     api: {
-        errorType: Config.withType<ErrorTypeConfig>().defineEntity({
-            errorMode: 'exit',
+        errorsConfig: Config.withType<ErrorsConfig>().defineEntity({
+            throwError: 'exit',
             handleUncaught: false,
         }),
     },
--- test-fixtures/disconnecting-env/src/disconnecting-env.server.env.ts
+++ test-fixtures/disconnecting-env/src/disconnecting-env.server.env.ts
@@ -1,14 +1,21 @@
 import testFeature, { serverEnv } from './disconnecting-env.feature';
 
-testFeature.setup(serverEnv, ({ run, errorType: { errorMode } }) => {
+testFeature.setup(serverEnv, ({ run, errorsConfig: { throwError, handleUncaught } }) => {
+    if (handleUncaught) {
+        process.on('uncaughtException', () => {
+            // User can do any other cleanup here
+            process.exit(1);
+        });
+    }
+
     run(() => {
-        if (errorMode === 'exception') {
+        if (throwError === 'exception') {
             throw new Error('Throwing a regular exception');
         }
-        if (errorMode === 'exit') {
+        if (throwError === 'exit') {
             process.exit(1);
         }
-        if (errorMode === 'promiseReject') {
+        if (throwError === 'promise-reject') {
             void Promise.reject('promise reject');
         }
     });
