3b44bbbccde0c1a48197e3a8e2eaa68931a0cf48
Oleh Rak <olehr@wix.com>
2023-04-14 15:00:38 +0300

    fixed review comments

--- packages/electron-commons/test/worker-thread.spec.ts
+++ packages/electron-commons/test/worker-thread.spec.ts
@@ -21,23 +21,11 @@ const setupRunningEnv = (featureId: string) =>
 describe('workerthread environment type', () => {
     it('initializes and exposes API', async () => {
         const { exitPromise } = await setupRunningEnv(workerThreadFeature.id);
-
-        const exitResult = await exitPromise;
-
-        expect(exitResult.exitCode).to.eql(0);
+        await expect(exitPromise).to.eventually.have.property('exitCode').eql(0);
     });
+
     it('initializes multi workerthread environment and exposes API', async () => {
         const { exitPromise } = await setupRunningEnv(`${workerThreadFeature.id}/multi`);
-
-        const exitResult = await exitPromise;
-
-        expect(exitResult.exitCode).to.eql(0);
-    });
-    xit('initializes multi workerthread environment and exposes API if called from async method', async () => {
-        const { exitPromise } = await setupRunningEnv(`${workerThreadFeature.id}/multi-async-get`);
-
-        const exitResult = await exitPromise;
-
-        expect(exitResult.exitCode).to.eql(0);
+        await expect(exitPromise).to.eventually.have.property('exitCode').eql(0);
     });
 });
