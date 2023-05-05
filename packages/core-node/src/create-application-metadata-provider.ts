dd68de6fa4c6aa8a5056a6fd6fe772db9ac627b0
Oleh Rak <olehr@wix.com>
2023-05-05 15:17:40 +0300

    move logic to single function to avoid double-memoization

--- packages/core-node/src/create-application-metadata-provider.ts
+++ packages/core-node/src/create-application-metadata-provider.ts
@@ -10,44 +10,15 @@ import memoizeOne from 'memoize-one';
  * @param com The communication instance that have registered hosts for `ROOT_ENGINE_ENV_ID` and `METADATA_PROVIDER_ENV_ID`
  */
 export function createMetadataProvider(com: Communication) {
-    const metadataProviderCom = createMetadataProviderCommunication(com);
-    const { metadata, disposeCommunication } = getMetadataFromCommunication(metadataProviderCom);
-
+    const { metadataPromise, disposeCommunication } = loadMetadata(com);
     return {
-        getMetadata: () => metadata,
+        getMetadata: () => metadataPromise,
         dispose: disposeCommunication,
     };
 }
 
-// memoize function to call api only once
-const getMetadataFromCommunication = memoizeOne((metadataProviderCom: Communication) => {
-    const api = metadataProviderCom.apiProxy<MetadataCollectionAPI>(
-        {
-            id: ENGINE_ROOT_ENVIRONMENT_ID,
-        },
-        metadataApiToken
-    );
-
-    const metadata = api.getRuntimeArguments();
-
-    // use disposables to ignore multiple dispose calls
-    const disposables = createDisposables();
-    disposables.add(() => metadataProviderCom.dispose());
-
-    // dispose metadataProviderCom once metadata is obtained
-    // it is done cause we don't need communication anymore once result memoized
-    // this might help with issue when client did not call dispose when exiting
-    // and this communication instance hangs process by being subscribed for events
-    metadata.then(() => disposables.dispose()).catch(reportError);
-
-    return {
-        metadata,
-        disposeCommunication: disposables.dispose,
-    };
-});
-
-// memoize function to create single communication instance per env
-const createMetadataProviderCommunication = memoizeOne((communication: Communication) => {
+// memoize function to instantiate single communication instance and call api only once
+const loadMetadata = memoizeOne((communication: Communication) => {
     const rootHost = communication.getEnvironmentHost(ENGINE_ROOT_ENVIRONMENT_ID);
     if (!rootHost) {
         throw new Error(
@@ -65,5 +36,27 @@ const createMetadataProviderCommunication = memoizeOne((communication: Communica
     const metadataProviderCom = new Communication(metadataProviderHost, METADATA_PROVIDER_ENV_ID);
     metadataProviderCom.registerEnv(ENGINE_ROOT_ENVIRONMENT_ID, rootHost);
 
-    return metadataProviderCom;
+    const api = metadataProviderCom.apiProxy<MetadataCollectionAPI>(
+        {
+            id: ENGINE_ROOT_ENVIRONMENT_ID,
+        },
+        metadataApiToken
+    );
+
+    const metadataPromise = api.getRuntimeArguments();
+
+    // use disposables to ignore multiple dispose calls
+    const disposables = createDisposables();
+    disposables.add(() => metadataProviderCom.dispose());
+
+    // dispose metadataProviderCom once metadata is obtained
+    // it is done cause we don't need communication anymore once result memoized
+    // this might help with issue when client did not call dispose when exiting
+    // and this communication instance hangs process by being subscribed for events
+    metadataPromise.then(() => disposables.dispose()).catch(reportError);
+
+    return {
+        metadataPromise,
+        disposeCommunication: disposables.dispose,
+    };
 });
