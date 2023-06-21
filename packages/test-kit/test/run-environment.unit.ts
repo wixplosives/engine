3830cd0a405ec97937f72aab5b5312e945af0424
Omry Nachman <omry@wix.com>
2023-06-21 12:50:32 +0300

    chore: updated getRunningFeature to disposeAfter by default

--- packages/test-kit/test/run-environment.unit.ts
+++ packages/test-kit/test/run-environment.unit.ts
@@ -4,22 +4,17 @@ import fs from '@file-services/node';
 
 import { getRunningFeature } from '../src/run-environment';
 import workerThreadFeature, { serverEnv } from '@fixture/worker-thread/dist/worker-thread.feature';
-import { createDisposables } from '@wixc3/create-disposables';
 
 const featurePath = fs.dirname(require.resolve('@fixture/worker-thread/package.json'));
 
 describe('runs environment', () => {
-    const disposables = createDisposables();
-    afterEach(disposables.dispose);
-
     it('runs environment with workerthread support', async () => {
-        const { dispose, runningApi } = await getRunningFeature({
+        const { runningApi } = await getRunningFeature({
             env: serverEnv,
             feature: workerThreadFeature,
             featureName: workerThreadFeature.id,
             basePath: featurePath,
         });
-        disposables.add(dispose);
 
         const result = await runningApi.workerService.initAndCallWorkerEcho('hello');
         expect(result).to.equal('hello from worker');
