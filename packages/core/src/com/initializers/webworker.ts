db16fc83a22b0ad3b8bb5f7e3c9b4dddb823eab8
Barak Igal <baraki@wix.com>
2023-06-29 13:45:39 +0300

    feat: module type worker

--- packages/core/src/com/initializers/webworker.ts
+++ packages/core/src/com/initializers/webworker.ts
@@ -3,13 +3,16 @@ import { Worker } from '@wixc3/isomorphic-worker/worker';
 import { UniversalWorkerHost } from '../hosts/universal-worker-host';
 import type { InitializerOptions } from './types';
 
-export async function webWorkerInitializer({ communication, env: { env, endpointType } }: InitializerOptions) {
-    const isSingleton = endpointType === 'single';
-    const instanceId = isSingleton ? env : communication.generateEnvInstanceID(env);
-
-    const webWorker = new Worker(`${communication.getPublicPath()}${env}.webworker.mjs${location.search}`, {
+export async function webWorkerInitializer({
+    communication,
+    env: { env, endpointType },
+    mjs,
+}: InitializerOptions & { mjs?: true }) {
+    const instanceId = communication.getEnvironmentInstanceId(env, endpointType);
+    const url = `${communication.getPublicPath()}${env}.webworker.${mjs ? 'mjs' : 'js'}${location.search}`;
+    const webWorker = new Worker(url, {
         name: instanceId,
-        type: 'module',
+        type: mjs ? 'module' : 'classic',
     });
 
     const host = new UniversalWorkerHost(webWorker, instanceId);
