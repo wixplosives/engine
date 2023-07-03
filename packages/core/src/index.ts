ee2f5729e9848d60dda4ba7b680887f1c27f5d6a
Omry Nachman <omry@wix.com>
2023-07-03 10:44:23 +0300

    Revert "refactor: change code from strings to actual code (#1870)"
    This reverts commit ae5d165667dc4bbb7c6f271273e3c3a5f7a6328d.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -11,9 +11,6 @@ export * from './types';
 export * from './communication.feature';
 export * from './runtime-metadata.feature';
 
-export * from './runtime-main';
-export * from './runtime-configurations';
-
 import COM from './communication.feature';
 export { COM };
 
