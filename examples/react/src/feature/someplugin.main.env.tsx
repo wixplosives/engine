e8919caf7f81c516ee65f00ffca8858199a666fc
Matan Georgi <georgimatan@gmail.com>
2023-07-30 17:16:13 +0300

    test: check persist option

--- examples/react/src/feature/someplugin.main.env.tsx
+++ examples/react/src/feature/someplugin.main.env.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Comp } from './someComp';
 
 somepluginFeature.setup(MainEnv, ({}, { guiFeature: { extentionSlot } }) => {
+    extentionSlot.register(<input type="checkbox" key="checkbox" id="checkbox" />);
     extentionSlot.register(<li key={somepluginFeature.id}>thiss registered</li>);
     extentionSlot.register(<Comp key="cosmp" />);
 });
---
+++ examples/react/src/test/react.persist.it.ts
@@ -0,0 +1,31 @@
+import { withFeature } from '@wixc3/engine-test-kit';
+import { join } from 'path';
+import { Page } from 'playwright-core';
+
+describe('Persist option', function () {
+    this.timeout(20_000);
+
+    const { getLoadedFeature } = withFeature({
+        featureName: 'react/someplugin',
+        runOptions: {
+            projectPath: join(__dirname, '..'),
+        },
+        persist: true,
+    });
+
+    let page: Page;
+    before(async function () {
+        ({ page } = await getLoadedFeature());
+    });
+
+    it('Check checkbox in order for it to appear in the next test', async () => {
+        const checkbox = page.locator('#checkbox');
+        await checkbox.waitFor();
+        await checkbox.check();
+    });
+
+    it('Validate if checkbox is checked from the previous test', async () => {
+        const checkbox = page.locator('#checkbox');
+        await checkbox.isChecked({ timeout: 1000 });
+    });
+});
