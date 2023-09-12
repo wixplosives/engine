ce87b0f5e1cc0da15671c100c2be6b81d3dacc37
Barak <baraki@wix.com>
2023-09-12 15:43:06 +0300

    feat: avoid analyzing type only imports and exports.

--- packages/scripts/src/utils/resolve-module-graph.ts
+++ packages/scripts/src/utils/resolve-module-graph.ts
@@ -60,8 +60,8 @@ function extractModuleRequests(sourceFile: ts.SourceFile): string[] {
     };
 
     const importsFinder = (node: ts.Node) => {
-        const isNonTypeImport = isImportDeclaration(node) && !node.importClause?.isTypeOnly;
-        const isNonTypeExport = isExportDeclaration(node) && !node.isTypeOnly;
+        const isNonTypeImport = isImportDeclaration(node) && !isTypeOnlyImport(node);
+        const isNonTypeExport = isExportDeclaration(node) && !isTypeOnlyExports(node);
 
         if ((isNonTypeImport || isNonTypeExport) && node.moduleSpecifier && isStringLiteral(node.moduleSpecifier)) {
             const originalTarget = node.moduleSpecifier.text;
@@ -76,6 +76,26 @@ function extractModuleRequests(sourceFile: ts.SourceFile): string[] {
 
     forEachChild(sourceFile, importsFinder);
     return specifiers;
+}
+
+
+function isTypeOnlyExports(node: ts.ExportDeclaration) {
+    return node.isTypeOnly || hasOnlyTypeBindings(node.exportClause);
+}
+
+function isTypeOnlyImport(node: ts.ImportDeclaration) {
+    return node.importClause?.isTypeOnly || hasOnlyTypeBindings(node.importClause?.namedBindings);
+}
+
+function hasOnlyTypeBindings(bindings?: ts.NamedImportBindings | ts.NamedExportBindings) {
+    if (bindings?.kind === ts.SyntaxKind.NamedImports || bindings?.kind === ts.SyntaxKind.NamedExports) {
+        for (const element of bindings.elements) {
+            if (!element.isTypeOnly) {
+                return false;
+            }
+        }
+    }
+    return false;
 }
 
 function isRequireIdentifier(expression: ts.Expression): expression is ts.Identifier {
