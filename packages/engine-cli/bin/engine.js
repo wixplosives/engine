6a996dba426df3f6db11a6c67075f07271b9fa32
Barak <baraki@wix.com>
2023-11-14 12:07:10 +0200

    feat(engine-cli): run with engine config flags

--- packages/engine-cli/bin/engine.js
+++ packages/engine-cli/bin/engine.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-require('../dist/cli.js');
+const { resolveExecArgv } = require('@wixc3/engine-scripts');
+const { once } = require('node:events');
+const { Worker } = require('node:worker_threads');
+
+(async () => {
+    const basePath = process.cwd();
+    const execArgv = await resolveExecArgv(basePath);
+    const worker = new Worker(require.resolve('../dist/cli.js'), {
+        argv: process.argv.slice(2),
+        execArgv,
+    });
+    const [code] = await once(worker, 'exit');
+    process.exit(code);
+})().catch((err) => {
+    console.log(err);
+    process.exit(1);
+});
