c4d890bae2264cd3af8b456b5a8138d0e3c6a7a5
Barak <baraki@wix.com>
2023-11-23 01:02:39 +0200

    fix: param handling in cli

--- packages/engine-cli/src/cli.ts
+++ packages/engine-cli/src/cli.ts
@@ -2,18 +2,17 @@ import { loadEngineConfig, runEngine, parseCliArgs } from './engine-build';
 
 async function engine() {
     const args = parseCliArgs();
-    const help = Boolean(args.has('help')) ?? false;
     const buildTargets = (args.get('buildTargets') as 'node' | 'web' | 'both') ?? 'both';
-    const clean = Boolean(args.get('clean')) ?? true;
-    const watch = Boolean(args.get('watch')) ?? false;
-    const dev = Boolean(args.get('dev')) ?? watch;
-    const run = Boolean(args.get('run')) ?? dev;
-    const feature = String(args.get('feature'));
-    const config = String(args.get('config'));
-    const publicPath = String(args.get('publicPath')) ?? '';
-    const verbose = Boolean(args.get('verbose')) ?? false;
-    const engineConfigFilePath = String(args.get('engineConfigFilePath'));
-
+    const help = boolParam(args.has('help')) ?? false;
+    const clean = boolParam(args.get('clean')) ?? true;
+    const watch = boolParam(args.get('watch')) ?? false;
+    const dev = boolParam(args.get('dev')) ?? watch;
+    const run = boolParam(args.get('run')) ?? dev;
+    const verbose = boolParam(args.get('verbose')) ?? false;
+    const feature = strParam(args.get('feature'));
+    const config = strParam(args.get('config'));
+    const publicPath = strParam(args.get('publicPath')) ?? '';
+    const engineConfigFilePath = strParam(args.get('engineConfigFilePath'));
     if (help) {
         console.log(engine.toString());
         console.log('🤷‍♂️');
@@ -21,6 +20,13 @@ async function engine() {
 
     const engineConfig = await loadEngineConfig(process.cwd(), engineConfigFilePath);
     await runEngine({ engineConfig, verbose, clean, dev, watch, publicPath, buildTargets, feature, config, run });
+}
+
+function strParam(param?: string | boolean) {
+    return param !== undefined ? String(param) : undefined;
+}
+function boolParam(param?: string | boolean) {
+    return param !== undefined ? Boolean(param) : undefined;
 }
 
 engine().catch((e) => {
