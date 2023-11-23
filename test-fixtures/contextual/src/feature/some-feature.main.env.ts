e5e2a34641d251efa5758c9eeac020240fa9f011
Barak <baraki@wix.com>
2023-11-23 12:58:04 +0200

    fix: force test to use classes web worker

--- test-fixtures/contextual/src/feature/some-feature.main.env.ts
+++ test-fixtures/contextual/src/feature/some-feature.main.env.ts
@@ -4,10 +4,17 @@ import sampleFeature from './some-feature.feature.js';
 
 sampleFeature.setup(mainEnv, ({ run, serverService }, { COM: { communication } }) => {
     run(async () => {
+        /**
+         * This the engine we bundle from cjs dist so webpack uses importScripts.
+         * Our default webworker initializer uses esm so we need to override it.
+         */
         await initializeContextualEnv({
             communication,
             env: contextualEnv,
-            envInitializers: { server: socketClientInitializer, webworker: webWorkerInitializer },
+            envInitializers: {
+                server: socketClientInitializer,
+                webworker: (opt) => webWorkerInitializer({ ...opt, workerOptions: { type: 'classic' } }),
+            },
         });
 
         document.body.innerText = await serverService.echo();
