e1a73ebadb9ddfd000e2af6fd763be4f7e19c41c
Oleh Rak <olehr@wix.com>
2023-12-08 13:35:58 +0200

    fixed review comment

--- packages/core/src/polyfills/report-error-polyfill.ts
+++ packages/core/src/polyfills/report-error-polyfill.ts
@@ -1,9 +1,12 @@
 import { reportError } from "../com";
 
 /**
- * hope this is single polyfill we need for now
- * in case if more will be added, we need to create some
- * better mechanism to handle polyfills
+ * This polyfill is not essential for the engine itself. But applications built
+ * on the engine may require this polyfill to correctly report initialization
+ * errors in older browsers.
+ * Ideally, the engine should offer a mechanism allowing applications to
+ * initialize necessary polyfills before the evaluation of feature files begins.
+ * But so far we only need this single polyfill.
  */
 if (globalThis.reportError === undefined) {
     globalThis.reportError = reportError;
