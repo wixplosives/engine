58381d1cb297f595ff318600d7db1552a632141d
Barak <baraki@wix.com>
2023-12-24 17:28:05 +0200

    refactor

--- packages/core/src/com/communication.ts
+++ packages/core/src/com/communication.ts
@@ -117,9 +117,10 @@ export class Communication {
      * Registers environments that spawned in the same execution context as the root environment.
      */
     public registerEnv(id: string, host: Target): void {
-        if (!this.environments[id]) {
+        const existingEnv = this.environments[id];
+        if (!existingEnv) {
             this.environments[id] = { id, host } as EnvironmentRecord;
-        } else if (this.environments[id]?.host !== host) {
+        } else if (existingEnv.host !== host) {
             throw new Error(DUPLICATE_REGISTER(id, 'Environment'));
         }
     }
