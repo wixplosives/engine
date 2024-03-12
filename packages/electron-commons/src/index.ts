28aeefd50e31fbd8d24160121c853a38b543b6d7
Pavlo Lisovyi <pavlol@wix.com>
2024-03-12 11:27:57 +0200

    feat(electron): remove unused and unmaintained electron-related code (#2361)
    BREAKING CHANGE: Removal of `engine-electron-scripts` and `engine-electron-node` packages and parts of `engine-electron` and `electron-host` packages

--- packages/electron-commons/src/index.ts
+++ packages/electron-commons/src/index.ts
@@ -1,4 +1,2 @@
 export * from './types.js';
 export * from './initializers/index.js';
-export * from './communication-channels.js';
-export { default as communicationChannels } from './communication-channels.js';
