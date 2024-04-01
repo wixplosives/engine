13e6fceeb4e07ad8380fc503ac7b4c745c01577e
Barak <baraki@wix.com>
2024-04-01 04:10:01 +0300

    feat: new way to see dependencies of features

---
+++ packages/engine-cli/src/analyze-command.ts
@@ -0,0 +1,30 @@
+import fs from '@file-services/node';
+import { EngineConfig, analyzeFeatures } from '@wixc3/engine-scripts';
+
+export async function analyzeCommand({
+    engineConfig,
+    rootDir,
+    feature,
+}: {
+    engineConfig: EngineConfig;
+    rootDir: string;
+    feature?: string;
+}) {
+    const { extensions, buildConditions, featureDiscoveryRoot } = engineConfig;
+
+    const { features } = await analyzeFeatures(fs, rootDir, featureDiscoveryRoot, feature, extensions, buildConditions);
+    const htmlTemplate = fs.readFileSync(fs.join(__dirname, 'dashboard/feature-walker.html'), 'utf8');
+    const replaceString = 'globalThis.EMBEDDED_DATA = new Map();';
+    if (!htmlTemplate.includes(replaceString)) {
+        throw new Error(`Cannot find "${replaceString}" in the feature-walker.html template`);
+    }
+
+    fs.writeFileSync(fs.join(rootDir, 'analyze.features.json'), JSON.stringify(Array.from(features), null, 2));
+    fs.writeFileSync(
+        fs.join(rootDir, 'analyze.features.html'),
+        htmlTemplate.replace(
+            replaceString,
+            `globalThis.EMBEDDED_DATA = new Map(${JSON.stringify(Array.from(features))});`,
+        ),
+    );
+}
