c5eae0130bf937dccd36c198054bb679da462a12
barak igal <baraki@wix.com>
2024-04-04 12:02:51 +0300

    feat: implement an engine feature dependencies walker  (#2423)

--- packages/engine-cli/src/cli.ts
+++ packages/engine-cli/src/cli.ts
@@ -1,7 +1,25 @@
-import { cli } from 'cleye';
+import { cli, command } from 'cleye';
+import { analyzeCommand } from './analyze-command';
 
 async function engine() {
     const argv = cli({
+        commands: [
+            command({
+                name: 'analyze',
+                flags: {
+                    feature: {
+                        type: String,
+                        description: 'Feature name',
+                        default: undefined,
+                    },
+                    engineConfigFilePath: {
+                        type: String,
+                        description: 'Engine config file path',
+                        default: undefined,
+                    },
+                },
+            }),
+        ],
         name: 'engine',
         help: {
             usage: false,
@@ -104,20 +122,26 @@ async function engine() {
             },
         },
     } as const);
-
-    const dev = argv.flags.dev ?? argv.flags.watch;
-    const run = argv.flags.run ?? dev;
-    const configLoadingMode = argv.flags.configLoadingMode ?? (argv.flags.watch ? 'watch' : 'require');
-
     const { loadEngineConfig, runEngine } = await import('./engine-build');
 
-    await runEngine({
-        ...argv.flags,
-        dev,
-        run,
-        configLoadingMode,
-        engineConfig: await loadEngineConfig(process.cwd(), argv.flags.engineConfigFilePath),
-    });
+    const engineConfig = await loadEngineConfig(process.cwd(), argv.flags.engineConfigFilePath);
+    const rootDir = process.cwd();
+    if (argv.command === 'analyze') {
+        await analyzeCommand({ rootDir, feature: argv.flags.feature, engineConfig });
+    } else {
+        const dev = argv.flags.dev ?? argv.flags.watch;
+        const run = argv.flags.run ?? dev;
+        const configLoadingMode = argv.flags.configLoadingMode ?? (argv.flags.watch ? 'watch' : 'require');
+
+        await runEngine({
+            rootDir,
+            ...argv.flags,
+            dev,
+            run,
+            configLoadingMode,
+            engineConfig,
+        });
+    }
 }
 
 engine().catch((e) => {
