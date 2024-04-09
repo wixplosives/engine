dc00b6b21fd29a37e3463165c56de825f213f418
Avi Vahl <avi@vahl.co.il>
2024-04-10 00:19:57 +0300

    chore(deps-dev): upgrade to latest stable and regenerate lock file

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -2,5 +2,6 @@ export default {
     pinnedPackages: [
         { name: 'open', reason: 'v9 is pure esm' },
         { name: 'chai', reason: 'v5 is pure esm' },
+        { name: 'eslint', reason: 'plugins are not yet compatible with v9' },
     ],
 };
