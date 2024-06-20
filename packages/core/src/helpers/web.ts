132bca6229e1ab122a30127524dcd6a99808f226
Katerina Zakimatova <ezakimatova@gmail.com>
2024-06-20 12:39:20 +0200

    allow-passing-entry-options-via-script-query

--- packages/core/src/helpers/web.ts
+++ packages/core/src/helpers/web.ts
@@ -9,3 +9,22 @@ export function injectScript(win: Window, rootComId: string, scriptUrl: string) 
         win.document.head.appendChild(scriptEl);
     });
 }
+
+export function getEngineEntryOptions(envName: string) {
+    const urlParams = new URLSearchParams(globalThis.location.search);
+    const currentScript = globalThis.document?.currentScript
+    const isHtmlScript = !!currentScript && 'src' in currentScript;
+
+    const scriptQueryString = isHtmlScript && currentScript.src?.split?.('?')?.[1] || ''
+    const scriptUrlParams = new URLSearchParams(scriptQueryString)
+    const injectedOptions = globalThis.engineEntryOptions?.({ urlParams, envName }) ?? new URLSearchParams('');
+
+    const definedParams = new Set<string>()
+    return new URLSearchParams([...injectedOptions, ...urlParams, ...scriptUrlParams].filter(([key]) => {
+        if (definedParams.has(key)) {
+            return false
+        }
+        definedParams.add(key)
+        return true
+    }))
+}
