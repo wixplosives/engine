5c56292f61dd1fbe6b23f16bc01461dab6fb173c
matan georgi <matang@wix.com>
2024-07-02 14:25:10 +0300

    feat: report error which are not EnvironmentDisconnected

--- packages/core/src/com/logs.ts
+++ packages/core/src/com/logs.ts
@@ -1,3 +1,4 @@
+import { EnvironmentDisconnectedError } from './environment-disconnected-error.js';
 import type { Message } from './message-types.js';
 
 export const DUPLICATE_REGISTER = (id: string, type: 'RemoteService' | 'Environment') =>
@@ -52,6 +53,11 @@ export function reportError(e: unknown) {
     console.error(e);
 }
 
+export function reportNonEnvironmentDisconnectedError(e: unknown) {
+    if (!(e instanceof EnvironmentDisconnectedError)) {
+        reportError(e);
+    }
+}
 
 export function UN_CONFIGURED_METHOD(api: string, method: string): string | undefined {
     return `cannot add listener to un-configured method ${api} ${method}`;
