051e744cef367bb3b1b677859dc576cf7a99bed2
Avi Vahl <avi@vahl.co.il>
2024-07-26 16:42:40 +0300

    infra: ignore sinon-chai@4 and regenerate lock file

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -3,6 +3,7 @@ export default {
         { name: 'open', reason: 'v9 is pure esm' },
         { name: 'chai', reason: 'v5 is pure esm' },
         { name: 'chai-as-promised', reason: 'v8 is pure esm' },
+        { name: 'sinon-chai', reason: 'v4 requires chai@5' },
         { name: 'eslint', reason: 'plugins are not yet compatible with v9' },
     ],
 };
