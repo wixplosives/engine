4e0781c6d938d243b33c38c37339ef95a4ee7295
Avi Vahl <avi.vahl@wix.com>
2024-08-15 14:50:40 +0300

    infra: re-enable lint for js/mjs/cjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,35 +6,40 @@ import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 import pluginTypescript from 'typescript-eslint';
 
+for (const config of pluginTypescript.configs.recommendedTypeChecked) {
+    config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
+}
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { ignores: ['**/dist/', '**/dist-engine/', '**/*.{js,mjs,cjs}', 'packages/engineer/gui-feature.d.ts'] },
+    { ignores: ['**/dist/', '**/dist-engine/', 'packages/engineer/gui-feature.d.ts'] },
     pluginJs.configs.recommended,
-
-    ...pluginTypescript.configs.recommendedTypeChecked,
-    { languageOptions: { parserOptions: { projectService: true } } },
-
     pluginReact.configs.flat.recommended,
     // pluginReact.configs.flat['jsx-runtime'],
     { settings: { react: { version: 'detect' } } },
-
     {
         plugins: {
             'react-hooks': pluginReactHooks,
             'no-only-tests': fixupPluginRules(pluginNoOnlyTests),
         },
     },
-    configPrettier,
-
     {
         rules: {
-            // "no-console": "error",
+            // 'no-console': 'error',
             'no-empty-pattern': 'off',
+            'no-only-tests/no-only-tests': 'error',
+            'no-undef': 'off',
+            'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
             'react/prop-types': 'off',
             'react-hooks/rules-of-hooks': 'error',
             'react-hooks/exhaustive-deps': 'error',
-            'no-only-tests/no-only-tests': 'error',
-
+        },
+    },
+    ...pluginTypescript.configs.recommendedTypeChecked,
+    { languageOptions: { parserOptions: { projectService: true } } },
+    {
+        files: ['**/*.{ts,tsx,mts,cts}'],
+        rules: {
             '@typescript-eslint/no-empty-object-type': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unsafe-argument': 'off',
@@ -48,4 +53,5 @@ export default [
             '@typescript-eslint/unbound-method': 'off',
         },
     },
+    configPrettier,
 ];
