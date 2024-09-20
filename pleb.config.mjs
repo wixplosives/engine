cace2be51cb7188e36ef18c0123022a38436ef9a
Avi Vahl <avi@vahl.co.il>
2024-09-21 02:39:59 +0300

    chore(deps-dev): upgrade to latest stable and regenerate lock file

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -5,5 +5,6 @@ export default {
         { name: 'chai-as-promised', reason: 'v8 is pure esm' },
         { name: '@types/chai-as-promised', reason: 'v8 is pure esm' },
         { name: 'sinon-chai', reason: 'v4 requires chai@5' },
+        { name: 'typescript-eslint', reason: '8.6.0 has a crash to be fixed in next release' },
     ],
 };
