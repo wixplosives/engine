778c3ae46e41a366d0d0f4f0c72ff74d1039888d
Avi Vahl <avi@vahl.co.il>
2024-09-25 14:11:36 +0300

    infra: ignore esm-only upgrades

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,9 +1,11 @@
 export default {
     pinnedPackages: [
-        { name: 'open', reason: 'v9 is pure esm' },
-        { name: 'chai', reason: 'v5 is pure esm' },
-        { name: 'chai-as-promised', reason: 'v8 is pure esm' },
-        { name: '@types/chai-as-promised', reason: 'v8 is pure esm' },
-        { name: 'sinon-chai', reason: 'v4 requires chai@5' },
+        { name: 'open', reason: 'esm-only' },
+        { name: 'chai', reason: 'esm-only' },
+        { name: '@types/chai', reason: 'esm-only' },
+        { name: 'chai-as-promised', reason: 'esm-only' },
+        { name: '@types/chai-as-promised', reason: 'esm-only' },
+        { name: 'sinon-chai', reason: 'esm-only' },
+        { name: '@types/sinon-chai', reason: 'esm-only' },
     ],
 };
