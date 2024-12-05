ccdf8cbfa79bb6bd3ba1a17e5b76684fc9f455aa
Avi Vahl <avi.vahl@wix.com>
2024-12-05 12:46:02 +0200

    refactor: simply build-dashboard script

--- build-dashboard.mjs
+++ build-dashboard.mjs
@@ -1,11 +1,6 @@
 import fs from 'node:fs';
 fs.cpSync('./packages/engine-cli/src/dashboard', './packages/engine-cli/dist/dashboard', {
     recursive: true,
-    filter: (src) => {
-        if (src.endsWith('.ts')) {
-            return false;
-        }
-        return true;
-    },
+    filter: (src) => !src.endsWith('.ts'),
 });
 console.log('Copied dashboard to dist');
